0dcdef3daca6cd1f39e76db60abcc5d96ecdff6f
Ewen Malaquin <ewen.malaquin@gmail.com>
2024-01-22 11:48:13 +0100

    :sparkles: création des énums ValidationLetter et Color

--- src/main/java/com/zenika/game/ValidationLetter.java
+++ src/main/java/com/zenika/game/ValidationLetter.java
@@ -2,4 +2,7 @@ package com.zenika.game;
 
 public enum ValidationLetter {
 
+    GOOD_POSITION,
+    WRONG_POSITION,
+    NOT_IN_WORD
 }
--- src/main/java/com/zenika/game/displayer/console/color/Color.java
+++ src/main/java/com/zenika/game/displayer/console/color/Color.java
@@ -1,4 +1,12 @@
 package com.zenika.game.displayer.console.color;
 
 public enum Color {
+    RESET,
+    GREEN_BACKGROUND,
+    BLUE_BACKGROUND,
+    CYAN_BACKGROUND,
+    BLACK_WRITE;
+    protected String code;
+
+
 }
